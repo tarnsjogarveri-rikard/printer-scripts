c05b6e0bbafd3687901d5bfa4ca97eb793cda1c5
Rikard lindström <--help>
2023-04-13 16:43:24 +0200

    Change all batch files to use relative path

--- Skrivare - Vån 3 - Admin - Svart.bat
+++ Skrivare - Vån 3 - Admin - Svart.bat
@@ -1,3 +1,6 @@
+set printers=%~dp0
+set drivers=%printers:Skrivare=Drivrutiner%
+
 cscript %windir%\System32\Printing_Admin_Scripts\sv-SE\prnport.vbs -a -r IP_192.168.10.57 -h 192.168.10.57
-rundll32 printui.dll,PrintUIEntry /ia /f "\\tgsrv02\Filer\Drivrutiner\Skrivare\Brother Universal\eng\BHPCL5E.INF" /m "Brother PCL5e Driver"
-rundll32 printui.dll,PrintUIEntry /if /b "V†n 3 - Admin - Svart" /r "IP_192.168.10.57" /f "\\tgsrv02\Filer\Drivrutiner\Skrivare\Brother Universal\eng\BHPCL5E.INF" /m "Brother PCL5e Driver"
+rundll32 printui.dll,PrintUIEntry /ia /f "%drivers%\Skrivare\Brother Universal\eng\BHPCL5E.INF" /m "Brother PCL5e Driver"
+rundll32 printui.dll,PrintUIEntry /if /b "V†n 3 - Admin - Svart" /r "IP_192.168.10.57" /f "%drivers%\Skrivare\Brother Universal\eng\BHPCL5E.INF" /m "Brother PCL5e Driver"
